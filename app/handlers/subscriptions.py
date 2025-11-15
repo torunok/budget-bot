d22d3c425573aef1a92ee1a136043ae2c11f1396
torunok <romanmoloshiii@gmail.com>
2025-11-15 07:22:44 +0200

    Update subscriptions.py

--- app/handlers/subscriptions.py
+++ app/handlers/subscriptions.py
@@ -4,7 +4,7 @@
 
 import logging
 from datetime import datetime
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Any
 
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
